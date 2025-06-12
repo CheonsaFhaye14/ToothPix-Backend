9ce18355e35220188ba4e20eef33dca7d30b381c
CheonsaFhaye14 <angelfhayerafanan@gmail.com>
2025-06-12 22:56:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,9 @@ const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+console.log('GOOGLE_SERVICE_ACCOUNT:', process.env.GOOGLE_SERVICE_ACCOUNT ? 'Exists' : 'Not set');
+console.log('GOOGLE_SERVICE_ACCOUNT JSON:', process.env.GOOGLE_SERVICE_ACCOUNT);
+
   async function sendNotificationToUser(fcmToken, message) {
     try {
      await admin.messaging().send({
